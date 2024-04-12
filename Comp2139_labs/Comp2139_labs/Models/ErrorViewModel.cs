f511b17f1495c99c29fb5c64fa49bbf2a5f529a0
Om21o7 <122809880+Om21o7@users.noreply.github.com>
2024-04-12 14:35:24 -0400

    Delete Comp2139_labs directory

--- Comp2139_labs/Comp2139_labs/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Comp2139_labs.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
